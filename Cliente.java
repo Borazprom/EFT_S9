aafe82fb9f31db40381202dd96452d7333293296
Felipe Borbarán D <111892451+Borazprom@users.noreply.github.com>
2025-05-12 16:21:17 -0400

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,25 @@
+package com.mycompany.teatro_moro;
+
+public class Cliente {
+    private String nombre;
+    private int edad;
+    private String tipo; // "Normal", "Niño", "Mujer", "Estudiante", "Tercera Edad"
+
+    public Cliente(String nombre, int edad, String tipo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+}
